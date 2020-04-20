f89fbb1700e3597f211ccde4473fbaf63f71648f
Marcos <sirmarcos@gmail.com>
2020-04-20 08:14:12 -0400

    fix DC abbrevation, fix covid tracking function.

--- functions.R
+++ functions.R
@@ -24,9 +24,9 @@ get_state_data_covid_tracking <- function() {
     state_json <- merge(state_json, state_info, by = "state_abbr", all.x = TRUE) %>% rename(case_growth = positiveIncrease, cases=positive)
     #merge population data, create new columns
     pop_data <- read_csv('SCPRC-EST2019-18+POP-RES.csv')
-    state_data <- merge(state_data,pop_data,by.x='state',by.y='NAME',all.x=TRUE)
-    state_data <- mutate(state_data,cases_per_100K = 100000 * (cases / POPESTIMATE2019), case_growth_per_100K = 100000 * (case_growth / POPESTIMATE2019))
-    state_data <- state_data[order(state_data$date),]
+    state_json <- merge(state_json,pop_data,by.x='state',by.y='NAME',all.x=TRUE)
+    state_json <- mutate(state_json,cases_per_100K = 100000 * (cases / POPESTIMATE2019), case_growth_per_100K = 100000 * (case_growth / POPESTIMATE2019))
+    state_json <- state_json[order(state_json$date),]
     
     return(state_json)
 }
