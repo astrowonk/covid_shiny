ef79c11f8ede492515f9d8043d05c0dcf0baa85f
Marcos Huerta <sirmarcos@gmail.com>
2021-01-22 08:48:24 -0500

    download and pre-merge with R

--- functions.R
+++ functions.R
@@ -11,7 +11,7 @@ require(plotly)
 
 get_county_nyt <- function () {
     
-    county_data <- read_csv("data_cache/us-counties.csv")
+    county_data <-  read_csv(url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"))
     county_data$date <- as.Date(county_data$date)
     county_data <- county_data[order(county_data$date),]
     county_data <- county_data %>% group_by(county,state) %>% mutate(case_growth = cases - lag(cases))
@@ -229,7 +229,11 @@ plot_county_hospital <-
 
 get_state_data_nyt <- function() {
     state_data <-
-        read_csv("data_cache/us-states.csv")
+        read_csv(
+            url(
+                "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+            )
+        )
     state_data$date <- state_data$date %>% as.Date()
     state_data <- state_data[order(state_data$date), ]
     state_data <- state_data %>%
@@ -255,7 +259,8 @@ get_state_data_nyt <- function() {
 }
 
 get_state_data_covid_tracking <- function() {
-    state_json <-fromJSON("data_cache/daily.json")
+    state_json <-
+        fromJSON(txt = url("https://covidtracking.com/api/states/daily"))
     state_json$date <- as.Date(state_json$dateChecked)
     state_json <- state_json %>% rename(state_abbr = state)
     state_info <-
